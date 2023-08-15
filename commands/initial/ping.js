1d97f815f448f5ad7acc4ad7612f27ebf6dce9cc
Eyo Sitota <eyobed01@gmail.com>
2023-08-14 23:27:17 -0400

    add command deployment

--- commands/initial/ping.js
+++ commands/initial/ping.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require("discord.js");
 
-nodule.export = {
+nodule.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
--- index.js
+++ index.js
@@ -16,19 +16,20 @@ client.commands = new Collection();
 
 //Path to commands
 const commandsPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-//Array of .js files at commands
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));	
-
-
-for (const file of commandFiles){
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	//Set a new item in the collection with the key as the command name and the value as the exported module
-	if('data' in command && 'execute' in command){
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+for (const file of commandFolders){
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
 	}
 }
 
